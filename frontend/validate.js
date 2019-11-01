1bd546cd3cc3997c6d1ec9013409ce2be07cd6f2
chubako <alfonso@armeto.com>
2019-11-01 06:15:50 -0300

    resetting subject_id validation after user input

--- frontend/validate.js
+++ frontend/validate.js
@@ -1,6 +1,10 @@
 window.addEventListener('DOMContentLoaded', function(evt) {
-
   var _form = document.querySelector("form");
+  const _subject_id = document.getElementById('subject_id');
+
+  _subject_id.addEventListener('input', function(event) {
+    _subject_id.setCustomValidity("");
+  });
 
   _form.addEventListener('submit', function(event) {
     const _subject_id = document.getElementById('subject_id');
