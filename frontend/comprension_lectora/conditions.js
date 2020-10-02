b1583f853e5f39f5458a204ea8e408109e3b7c23
Álvaro Cabana <almadana@gmail.com>
2020-10-02 20:38:52 -0300

    prueba atenas2

--- frontend/comprension_lectora/conditions.js
+++ frontend/comprension_lectora/conditions.js
@@ -29,28 +29,23 @@ const conditions = {
       ]
     },
   
-    {title: 'Texto 2',
-      text: `   *** Facere autem ut saepe repellat. Et inventore possimus voluptas. Temporibus fuga nulla quia ut. Cumque ea ad commodi architecto. Ducimus aut sed ut.
+    {title: 'La democracia en Atenas',
+      text: `   
+  ***PARTE A 
+
+  Tucídides fue un historiador y militar que vivió en el siglo V a. C, durante la época de la Grecia clásica. Nació en Atenas. Durante la guerra del Peloponeso (del 431 a. C. al 404 a. C.), entre Atenas y Esparta, estuvo al mando de una flota cuya misión era proteger la ciudad de Anfípolis, en Tracia. No consiguió llegar a la ciudad a tiempo. Ésta cayó en manos del general espartano, Brásidas, lo que condenó a Tucídides a veinte años de exilio. Esta circunstancia le dio la oportunidad de recoger información bastante completa de los dos bandos en conflicto y la posibilidad de investigar para su obra Historia de la guerra del Peloponeso. 
+
+  Tucídides está considerado como uno de los grandes historiadores de la Antigüedad. Se centra más en las causas naturales y en la conducta de cada individuo que en el destino o en la intervención de los dioses para explicar la evolución de la historia. En su obra, los hechos no se presentan como meras anécdotas, sino que se explican tratando de descubrir los motivos que llevaron a los protagonistas a actuar del modo en que lo hicieron. El énfasis que Tucídides pone en la conducta de los individuos le lleva a veces a introducir discursos ficticios: éstos le ayudan a exponer las motivaciones de los personajes históricos. 
+
+  ***PARTE B 
+
+  Tucídides atribuye al dirigente ateniense Pericles (siglo V a. C.) el siguiente discurso en honor de los soldados caídos en el primer año de la guerra del Peloponeso. 
   
-      *** Dicta omnis mollitia hic architecto ea. Sunt suscipit qui labore. Rerum cumque dolor est voluptatem in sint velit. Dignissimos aut est aut quos. Vel nihil ipsa autem ratione fuga ut. Aliquam dolores consectetur qui doloremque unde dolor dolor.
-  
-      *** Dolores amet odit repudiandae voluptas molestiae. Velit aut temporibus omnis occaecati vel porro ratione dolorem. Iure ipsam labore suscipit voluptates. Voluptatem soluta reprehenderit ex est facere.
-  
-      *** Est repellat voluptatum dicta ipsum. Aspernatur numquam eaque est expedita quos facilis asperiores mollitia. Asperiores nam optio repellat voluptatum quod consequatur est. Qui repellendus laborum omnis velit temporibus voluptas veritatis. Nesciunt et non omnis exercitationem molestiae corrupti ad. Doloremque eos exercitationem sit vel rerum laboriosam quam.
-  
-      *** Dicta omnis mollitia hic architecto ea. Sunt suscipit qui labore. Rerum cumque dolor est voluptatem in sint velit. Dignissimos aut est aut quos. Vel nihil ipsa autem ratione fuga ut. Aliquam dolores consectetur qui doloremque unde dolor dolor.
-  
-      *** Dolores amet odit repudiandae voluptas molestiae. Velit aut temporibus omnis occaecati vel porro ratione dolorem. Iure ipsam labore suscipit voluptates. Voluptatem soluta reprehenderit ex est facere.
-  
-      Est repellat voluptatum dicta ipsum. Aspernatur numquam eaque est expedita quos facilis asperiores mollitia. Asperiores nam optio repellat voluptatum quod consequatur est. Qui repellendus laborum omnis velit temporibus voluptas veritatis. Nesciunt et non omnis exercitationem molestiae corrupti ad. Doloremque eos exercitationem sit vel rerum laboriosam quam.
-  
-      Dicta omnis mollitia hic architecto ea. Sunt suscipit qui labore. Rerum cumque dolor est voluptatem in sint velit. Dignissimos aut est aut quos. Vel nihil ipsa autem ratione fuga ut. Aliquam dolores consectetur qui doloremque unde dolor dolor.
-  
-      Dolores amet odit repudiandae voluptas molestiae. Velit aut temporibus omnis occaecati vel porro ratione dolorem. Iure ipsam labore suscipit voluptates. Voluptatem soluta reprehenderit ex est facere.
-  
-      Est repellat voluptatum dicta ipsum. Aspernatur numquam eaque est expedita quos facilis asperiores mollitia. Asperiores nam optio repellat voluptatum quod consequatur est. Qui repellendus laborum omnis velit temporibus voluptas veritatis. Nesciunt et non omnis exercitationem molestiae corrupti ad. Doloremque eos exercitationem sit vel rerum laboriosam quam.
-  
-      Repellendus ipsa eos pariatur. Rerum ducimus rem reiciendis totam porro. Eum quia beatae omnis illo earum voluptates qui minima. Reprehenderit numquam similique qui qui. Aspernatur quia omnis voluptatem dolore laudantium et.`,
+    "Nuestro sistema de gobierno no copia las leyes de los estados vecinos; nosotros somos más un ejemplo para otros que imitadores de los demás. Nuestro sistema se denomina democracia, ya que el gobierno no depende de unos pocos, sino de una mayoría. Nuestras leyes garantizan iguales derechos para todos en las cuestiones privadas, mientras que el prestigio en la vida pública depende más de los méritos que de la clase social. 
+    Tampoco la clase social impide a nadie llegar a ejercer cualquier cargo público (...). Y, al tiempo que no interferimos en las cuestiones privadas, respetamos la ley en los asuntos públicos. Obedecemos a quienes ponemos a desempeñar cargos públicos y obedecemos las leyes, en especial las dirigidas a la protección de los oprimidos y las leyes no escritas que supone una verdadera vergüenza infringir. 
+    Además, nos hemos procurado muchos placeres para el espíritu. Los juegos y sacrificios que celebramos durante todo el año y la elegancia de nuestras casas particulares constituyen una fuente diaria de placer que nos ayuda a olvidar cualquier preocupación; mientras que los numerosos habitantes de la ciudad atraen a Atenas productos de todo el mundo, de modo que a los atenienses los frutos de otros pueblos les son tan familiares como los suyos propios. "
+
+    Tucídides, Historia de la guerra del Peloponeso (adaptación) `,
       questions: [
         "b1) ---",
         "b2) ***",
