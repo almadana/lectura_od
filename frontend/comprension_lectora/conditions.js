0205af892a4e2f85a42609aff4b433df722fad3d
perezoso <almadana@gmail.com>
2020-10-13 16:54:22 -0300

    pregutnas cargadas

--- frontend/comprension_lectora/conditions.js
+++ frontend/comprension_lectora/conditions.js
@@ -1,47 +1,126 @@
-const conditions = {
+            const conditions = {
   'development': [
-    {title: 'Texto 1',
-      text: `    Facere autem ut saepe repellat. Et inventore possimus voluptas. Temporibus fuga nulla quia ut. Cumque ea ad commodi architecto. Ducimus aut sed ut.
-  
-      Dicta omnis mollitia hic architecto ea. Sunt suscipit qui labore. Rerum cumque dolor est voluptatem in sint velit. Dignissimos aut est aut quos. Vel nihil ipsa autem ratione fuga ut. Aliquam dolores consectetur qui doloremque unde dolor dolor.
-  
-      Dolores amet odit repudiandae voluptas molestiae. Velit aut temporibus omnis occaecati vel porro ratione dolorem. Iure ipsam labore suscipit voluptates. Voluptatem soluta reprehenderit ex est facere.
-  
-      Est repellat voluptatum dicta ipsum. Aspernatur numquam eaque est expedita quos facilis asperiores mollitia. Asperiores nam optio repellat voluptatum quod consequatur est. Qui repellendus laborum omnis velit temporibus voluptas veritatis. Nesciunt et non omnis exercitationem molestiae corrupti ad. Doloremque eos exercitationem sit vel rerum laboriosam quam.
-  
-      Dicta omnis mollitia hic architecto ea. Sunt suscipit qui labore. Rerum cumque dolor est voluptatem in sint velit. Dignissimos aut est aut quos. Vel nihil ipsa autem ratione fuga ut. Aliquam dolores consectetur qui doloremque unde dolor dolor.
-  
-      Dolores amet odit repudiandae voluptas molestiae. Velit aut temporibus omnis occaecati vel porro ratione dolorem. Iure ipsam labore suscipit voluptates. Voluptatem soluta reprehenderit ex est facere.
-  
-      Est repellat voluptatum dicta ipsum. Aspernatur numquam eaque est expedita quos facilis asperiores mollitia. Asperiores nam optio repellat voluptatum quod consequatur est. Qui repellendus laborum omnis velit temporibus voluptas veritatis. Nesciunt et non omnis exercitationem molestiae corrupti ad. Doloremque eos exercitationem sit vel rerum laboriosam quam.
-  
-      Dicta omnis mollitia hic architecto ea. Sunt suscipit qui labore. Rerum cumque dolor est voluptatem in sint velit. Dignissimos aut est aut quos. Vel nihil ipsa autem ratione fuga ut. Aliquam dolores consectetur qui doloremque unde dolor dolor.
-  
-      Dolores amet odit repudiandae voluptas molestiae. Velit aut temporibus omnis occaecati vel porro ratione dolorem. Iure ipsam labore suscipit voluptates. Voluptatem soluta reprehenderit ex est facere.
-  
-      Est repellat voluptatum dicta ipsum. Aspernatur numquam eaque est expedita quos facilis asperiores mollitia. Asperiores nam optio repellat voluptatum quod consequatur est. Qui repellendus laborum omnis velit temporibus voluptas veritatis. Nesciunt et non omnis exercitationem molestiae corrupti ad. Doloremque eos exercitationem sit vel rerum laboriosam quam.
-  
-      Repellendus ipsa eos pariatur. Rerum ducimus rem reiciendis totam porro. Eum quia beatae omnis illo earum voluptates qui minima. Reprehenderit numquam similique qui qui. Aspernatur quia omnis voluptatem dolore laudantium et.`,
+    {title: 'Texto 1  - LA REPRESENTACIÓN SERÁ LA TRAMPA',
+      text: `
+<p style="font-style:italic">La acción se desarrolla en un castillo junto a una playa en Italia. </p>
+<h4>Acto primero </h4>
+
+Lujoso salón de invitados de un hermoso castillo al lado de la playa. Puertas a derecha e izquierda. Mobiliario de salón en medio del escenario: un sofá, una mesa y dos sillones. Grandes ventanas al fondo. Noche estrellada. El escenario está a oscuras. Cuando se levanta el telón, se oye a unos hombres que conversan en voz alta tras la puerta de la izquierda. La puerta se abre y entran tres caballeros de esmoquin. Uno de ellos enciende la luz inmediatamente. Se dirigen hacia el centro en silencio y se sitúan alrededor de la mesa. Se sientan a la vez, Gál en el sillón de la izquierda, Turai en el de la derecha y Ádám en el sofá del medio. Silencio muy largo, casi violento. Se estiran cómodamente. Silencio. Después: 
+
+<dl>
+<dt>GÁL</dt>
+<dd>¿Por qué estás tan pensativo? </dd>
+<dt>TURAI</dt>
+<dd>Estoy pensando en lo difícil que es comenzar la representación de una obra de teatro. Presentar a todos los personajes principales al inicio, cuando todo empieza. </dd>
+<dt>ÁDÁM</dt>
+<dd>Me imagino que debe ser complicado. </dd>
+<dt>TURAI</dt>
+<dd>Es endiabladamente complicado. La obra de teatro empieza. El público se queda en silencio. Los actores salen al escenario y el tormento comienza. Es una eternidad; a veces pasa hasta un cuarto de hora antes de que el público averigüe quién es quién y qué hace ahí. </dd>
+<dt>GÁL</dt>
+<dd>¡Sí que tienes una mente peculiar! ¿No puedes olvidarte de tu profesión ni siquiera por un momento? </dd>
+<dt>TURAI</dt> 
+<dd>Imposible. </dd>
+<dt>GÁL</dt>
+<dd>No pasa ni media hora sin que te pongas a hablar de teatro, actores u obras. Hay más cosas en el mundo. </dd>
+<dt>TURAI</dt>
+<dd>No las hay. Soy dramaturgo. Ésa es mi maldición. </dd>
+<dt>GÁL</dt>
+<dd>No debes ser esclavo de tu profesión. </dd>
+<dt>TURAI</dt>
+<dd>Si no la dominas, eres su esclavo. No hay término medio. Créeme, no es fácil empezar bien una obra de teatro. Es uno de los problemas más arduos de la puesta en escena. Presentar a los personajes rápidamente. Fijémonos en esta escena de aquí, con nosotros tres. Tres caballeros de esmoquin. Supongamos que no suben al salón de este castillo señorial, sino a un escenario, justo cuando comienza la obra de teatro. Tendrían que hablar sobre toda una serie de temas sin interés hasta que pudiera saberse quiénes somos. ¿No sería mucho más fácil comenzar todo esto poniéndonos de pie y presentándonos a nosotros mismos? Se levanta. Buenas noches. Los tres estamos invitados en este castillo. Acabamos de llegar del comedor, donde hemos tomado una cena excelente y hemos bebido dos botellas de champán. <br>
+Mi nombre es Sándor Turai, soy autor teatral, llevo escribiendo obras de teatro desde hace treinta años, ésa es mi profesión. Punto y final. Tu turno. 
+<dt>GÁL</dt> 
+<dd><i>Se levanta.</i> Mi nombre es Gál, también soy autor teatral. También escribo obras de teatro en colaboración con este caballero aquí presente. Somos una pareja famosa de autores teatrales. En todos los carteles de las buenas comedias y operetas se lee: escrita por Gál y Turai. Naturalmente, ésta es también mi profesión. </dd>
+<dt>GÁL y TURAI</dt>
+<dd><i>A la vez.</i> Y este joven... </dd>
+<dt>ÁDÁM</dt>
+<dd><i>Se levanta.</i> Este joven es, si me lo permiten, Albert Ádám, veinticinco años, compositor. Escribí la música de la última opereta de estos dos amables caballeros. Éste es mi primer trabajo para el teatro. Estos dos ángeles veteranos me han descubierto y ahora, con su ayuda, me gustaría hacerme famoso. Gracias a ellos me han invitado a este castillo, gracias a ellos me han hecho el frac y el esmoquin. En otras palabras, por el momento, soy pobre y desconocido. Aparte de eso soy huérfano y me crió mi abuela. Ella ya falleció. Estoy solo en el mundo. No tengo ni nombre, ni fortuna. </dd>
+<dt>TURAI</dt>
+<dd>Pero eres joven. </dd>
+<dt>GÁL</dt>
+<dd>E inteligente. </dd>
+<dt>ÁDÁM</dt>
+<dd>Y estoy enamorado de la solista. </dd>
+<dt>TURAI</dt>
+<dd>No debiste añadir eso. Los espectadores lo habrían averiguado de todas formas. 
+Todos se sientan. </dd>
+<dt>TURAI</dt>
+<dd>Y bien, ¿no sería ésta la manera más sencilla de empezar una obra de teatro? </dd>
+<dt>GÁL</dt>
+<dd>Si nos permitiesen hacerlo, sería fácil escribir obras de teatro. </dd>
+<dt>TURAI</dt>
+<dd>Créeme, no es tan complicado. Piensa en todo ello como en... </dd>
+<dt>GÁL</dt>
+<dd>De acuerdo, de acuerdo, de acuerdo, no empieces a hablar de teatro otra vez. Estoy harto de ello. Ya hablaremos mañana, si quieres. </dd>
+<p style="font-style:italic">“La representación será la trampa” es el comienzo de una obra de teatro del dramaturgo húngaro Ferenc Molnár.</p>`,
       questions: [
-        {text: "a1) ---",
-          name: "a1",
+        {text: "<b>Pregunta 1/7</b><br>¿Qué estaban haciendo los personajes de la obra de teatro justo antes de que se levantase el telón? ",
+          name: "pA1",
           options: [
-            {value: "a uno", text: "a Uno..."},
-            {value: "a dos", text: "a dos..."},
+            {value: "a", text: "Acaban de cenar y de tomar champagne."},
+            {value: "b", text: "Conversan en voz alta tras la puerta."},
+            {value: "c", text: "Le hicieron a Ádám el frac y el esmoquin."},
+            {value: "d", text: "Se preparan para salir al escenario."}
           ],
         },
-        {text: "a2) ---",
-          name: "a2",
+        {text: '<b>Pregunta 2/7</b><br>  “Es una eternidad; a veces pasa hasta un cuarto de hora (...)”. <br>Según Turai, ¿por qué un cuarto de hora es “una eternidad”? ',
+          name: "pA2",
           options: [
-            {value: "a tres", text: "a tres..."},
-            {value: "a cuatro", text: "a cuatro..."},
+            {value: "a", text: "Es mucho tiempo para esperar a que el público se quede quieto en un teatro abarrotado."},
+            {value: "b", text: "Parece que se tarda un siglo en aclarar la situación al comienzo de una obra de teatro."},
+            {value: "c", text: "Siempre parece que a un dramaturgo le lleva mucho tiempo escribir el comienzo de una obra de teatro."},                
+            {value: "d", text: "Parece que el tiempo avanza lentamente cuando ocurre un suceso importante en una obra de teatro."}
+          ],
+        },
+        {text: "<b>Pregunta 3/7</b><br>En general, ¿qué está haciendo el dramaturgo Molnár en este fragmento?",
+          name: "pA3",
+          options: [
+            {value: "a", text: "Está mostrando el modo en que cada personaje va a resolver sus propios problemas."},
+            {value: "b", text: "Está haciendo que sus personajes demuestren cómo es una eternidad en una obra de teatro."},
+            {value: "c", text: "Está dando un ejemplo de una típica y tradicional escena inicial de una obra de teatro."},                
+            {value: "d", text: "Está utilizando a los personajes para representar uno de sus propios problemas de creación."}
+          ],
+        },
+        {text: "<b>Pregunta 4/7</b><br>¿Por qué Turai afirma “Soy dramaturgo. Ésa es mi maldición”?",
+          name: "pA4",
+          options: [
+            {value: "a", text: "Presentar a los problemas es complicado."},
+            {value: "b", text: "Le fastidia tener que escribir obras de teatro."},
+            {value: "c", text: "No tiene un punto medio, la domina o es su esclavo."},                
+            {value: "d", text: "La puesta en escena significa siempre un problema."}
+          ],
+        },
+        {text: "<b>Pregunta 5/7</b><br>¿Qué opción corresponde con una de las principales dificultades al leer esta obra teatral? ",
+          name: "pA5",
+          options: [
+            {value: "a", text: "Ser consciente de la distinción entre los personajes de la obra y los actores de la misma."},
+            {value: "b", text: "Entender que el desarrollo de la acción revela las intenciones de los personajes."},
+            {value: "c", text: "Interpretar las personalidades sugeridas por los diálogos de los personajes."},                
+            {value: "d", text: "Reconocer la relación existente entre los diálogos y las acotaciones del texto."}
+          ],
+        },
+        {text: "<b>Pregunta 6/7</b><br>¿Cuál de los tres personajes es el más joven?",
+          name: "pA6",
+          options: [
+            {value: "a", text: "Molnár."},
+            {value: "b", text: "Gál."},
+            {value: "c", text: "Turai."},                
+            {value: "d", text: "Adám."}
+          ],
+        },
+        {text: "<b>Pregunta 7/7</b><br>La metanarrativa es un tipo de literatura en el que un personaje o grupo de personajes son autoconscientes de su naturaleza. El narrador o los personajes sistemáticamente llaman la atención sobre su condición con el fin de plantear preguntas acerca de la relación entre la ficción y la realidad. <br>¿Crees que esta obra de teatro es un ejemplo de este tipo de ficción?",
+          name: "pA7",
+          options: [
+            {value: "a", text: "No, los personajes están vinculados con el teatro pero no tienen conciencia de su condición."},
+            {value: "b", text: "Sí, los actores reconocen su naturaleza en cada intervención."},
+            {value: "c", text: "No, los personajes no son actores en una obra de teatro."},                
+            {value: "d", text: "Sí, los actores son personajes en una obra de teatro y se dedican al teatro."}
           ],
         },
       ]
     },
-  
-    {title: 'La democracia en Atenas',
-      text: `   
+    {title: 'Texto 2 - La democracia en Atenas',
+      text: `
       <h3>PARTE A</h3>
   <p>
   Tucídides fue un historiador y militar que vivió en el siglo V a. C, durante la época de la Grecia clásica. Nació en Atenas. Durante la guerra del Peloponeso (del 431 a. C. al 404 a. C.), entre Atenas y Esparta, estuvo al mando de una flota cuya misión era proteger la ciudad de Anfípolis, en Tracia. No consiguió llegar a la ciudad a tiempo. Ésta cayó en manos del general espartano, Brásidas, lo que condenó a Tucídides a veinte años de exilio. Esta circunstancia le dio la oportunidad de recoger información bastante completa de los dos bandos en conflicto y la posibilidad de investigar para su obra Historia de la guerra del Peloponeso. 
@@ -59,16 +138,16 @@ const conditions = {
 
     Tucídides, Historia de la guerra del Peloponeso (adaptación) `,
       questions: [
-        {text: "<b>Pregunta 1</b><br>  Uno de los objetivos del discurso de la parte B era honrar a los soldados caídos en el primer año de la guerra del Peloponeso. <br>¿Cuál era OTRO de los objetivos de este discurso?",
+        {text: "<b>Pregunta 1/7</b><br>  Uno de los objetivos del discurso de la parte B era honrar a los soldados caídos en el primer año de la guerra del Peloponeso. <br>¿Cuál era OTRO de los objetivos de este discurso?",
           name: "pB1",
           options: [
-            {value: "a", text: "Explicar las ventajas de la democracia ateniense."}
+            {value: "a", text: "Explicar las ventajas de la democracia ateniense."},
             {value: "b", text: "Honrar a los soldados muertos"},
             {value: "c", text: "Atenas es el tema del discurso."},                
             {value: "d", text: "Desarrollar las causas de la derrota ateniense."}
           ],
         },
-        {text: "<b>Pregunta 2</b><br>¿Qué condenó a Tucídides al exilio?",
+        {text: "<b>Pregunta 2/7</b><br>¿Qué condenó a Tucídides al exilio?",
           name: "pB2",
           options: [
             {value: "a", text: "Que no pudiese lograr la victoria de los atenienses en Anfípolis."},
@@ -77,7 +156,7 @@ const conditions = {
             {value: "d", text: "Que desertase de los atenienses para luchar con los espartanos."},
           ],
         },
-        {text: "<b>Pregunta 3</b><br>¿Quién escribió el discurso de la Parte B?",
+        {text: "<b>Pregunta 3/7</b><br>¿Quién escribió el discurso de la Parte B?",
           name: "pB3",
           options: [
             {value: "a", text: "Pericles."},
@@ -86,7 +165,7 @@ const conditions = {
             {value: "d", text: "Un guerrero ateniense."},
           ],
         },
-        {text: "<b>Pregunta 4</b><br>Según el texto, ¿qué diferenciaba a Tucídides de los demás historiadores de su época? ",
+        {text: "<b>Pregunta 4/7</b><br>Según el texto, ¿qué diferenciaba a Tucídides de los demás historiadores de su época? ",
           name: "pB4",
           options: [
             {value: "a", text: "Escribía sobre personas corrientes, no sobre héroes."},
@@ -95,7 +174,7 @@ const conditions = {
             {value: "d", text: "Se centraba en lo que llevaba a las personas a actuar del modo en que lo hacían."},
           ],
         },
-        {text: "<b>Pregunta 5</b><br>Fíjate en este fragmento del texto, que se encuentra casi al final de la Parte B: <p>«Además, nos hemos procurado muchos placeres para el espíritu. Los juegos y sacrificios que celebramos durante todo el año y la elegancia de nuestras casas particulares constituyen una fuente diaria de placer que nos hace olvidar cualquier preocupación.»</p> ¿Cuál de estas frases resume mejor esta parte del texto? ",
+        {text: "<b>Pregunta 5/7</b><br>Fíjate en este fragmento del texto, que se encuentra casi al final de la Parte B: <p>«Además, nos hemos procurado muchos placeres para el espíritu. Los juegos y sacrificios que celebramos durante todo el año y la elegancia de nuestras casas particulares constituyen una fuente diaria de placer que nos hace olvidar cualquier preocupación.»</p> ¿Cuál de estas frases resume mejor esta parte del texto? ",
           name: "pB5",
           options: [
             {value: "a", text: "El sistema de gobierno de Atenas permite a cualquier persona elaborar leyes."},
@@ -104,7 +183,7 @@ const conditions = {
             {value: "d", text: "La vida pública y privada son consideradas como una misma cosa."},
           ],
         },
-        {text: "<b>Pregunta 6</b><br>La parte A de La democracia en Atenas sirve para:",
+        {text: "<b>Pregunta 6/7</b><br>La parte A de La democracia en Atenas sirve para:",
           name: "pB6",
           options: [
             {value: "a", text: "Explicar por qué Tucídides se exilió."},
@@ -113,7 +192,7 @@ const conditions = {
             {value: "d", text: "Describir la época en la cual Tucídides ejerció su profesión."},
           ],
         },
-        {text: "<b>Pregunta 7</b><br>¿Cuál de las descripciones del estilo de Tucídides corresponde con el siguiente fragmento de Historia de la guerra del Peloponeso? <p>«Obedecemos a quienes ponemos a desempeñar cargos públicos y obedecemos las leyes, en especial las dirigidas a la protección de los oprimidos y las leyes no escritas que supone una verdadera vergüenza infringir.»</p>",
+        {text: "<b>Pregunta 7/7</b><br>¿Cuál de las descripciones del estilo de Tucídides corresponde con el siguiente fragmento de Historia de la guerra del Peloponeso? <p>«Obedecemos a quienes ponemos a desempeñar cargos públicos y obedecemos las leyes, en especial las dirigidas a la protección de los oprimidos y las leyes no escritas que supone una verdadera vergüenza infringir.»</p>",
           name: "pB7",
           options: [
             {value: "a", text: "Tucídides fue un historiador y militar que vivió en el siglo V a. C, durante la época de la Grecia clásica."},
